945de01a493821c6e6ec680d5351f6181274aaa7
kurutto <namihojo@gmail.com>
2025-03-29 22:40:01 +0900

    重複していた型宣言をtypes.tsに一元管理#36

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,22 +1,8 @@
+import { UserType } from "@/types/types";
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: string;
-      name?: string | null;
-      email?: string | null;
-      emailVerified?: Date | null;
-      image?: string | null;
-      fridgeId?: string | null;
-    };
-  }
-
-  interface User {
-    id: string;
-    name?: string;
-    email?: string;
-    emailVerified?: Date;
-    image?: string;
+    user: UserType;
   }
 }
