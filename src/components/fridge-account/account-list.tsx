306cf705e567eec880552414e295ae7cc1dca2bc
kurutto <namihojo@gmail.com>
2025-03-29 23:02:56 +0900

    vercelのエラー修正、軽微な修正 Error: React Hook "useChangeFridgeAccount" is called conditionally. React Hooks must be called in the exact same order in every component render. Did you accidentally call a React Hook after an early return?  react-hooks/rules-of-hooks

--- src/components/fridge-account/account-list.tsx
+++ src/components/fridge-account/account-list.tsx
@@ -2,20 +2,30 @@
 import { FridgeType } from "@/types/types";
 import { useChangeFridgeAccount } from "@/hooks/useChangeFridgeAccount";
 import Button from "../ui/button";
+import { cn } from "@/lib/utils";
 
-const AccountList = ({
-  fridgeAccounts,
-}:{fridgeAccounts:FridgeType[]} ) => {
+interface AccountListProps 
+extends Omit<React.ComponentPropsWithoutRef<"ul">, "className"> {
+  fridgeAccounts: FridgeType[];
+  className?:string;
+}
+
+const AccountList = ({ fridgeAccounts, className, ...props }: AccountListProps) => {
+  const { changeFridgeAccount } = useChangeFridgeAccount();
   if (!fridgeAccounts) {
-    return;
+    return null;
   }
-  const {changeFridgeAccount} = useChangeFridgeAccount()
+  const baseStyle = "space-y-4 pt-4";
 
   return (
-    <ul className="space-y-4 pt-4">
+    <ul className={cn(baseStyle, className)} {...props}>
       {fridgeAccounts.map((fridgeAccount, idx) => (
         <li className="text-center" key={idx}>
-          <Button color="outline" className="min-w-52" onClick={()=>changeFridgeAccount(fridgeAccount.id)}>
+          <Button
+            color="outline"
+            className="min-w-52"
+            onClick={() => changeFridgeAccount(fridgeAccount.id)}
+          >
             {fridgeAccount.name}
           </Button>
         </li>
