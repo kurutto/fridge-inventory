2a3c71863aaa75cc1af5e373681583ab47d40ced
kurutto <namihojo@gmail.com>
2025-03-29 22:50:46 +0900

    vercelエラー修正#36 Error: 'res' is defined but never used.  @typescript-eslint/no-unused-vars Error: 'fridge' is assigned a value but never used.  @typescript-eslint/no-unused-vars Error: 'userFridge' is assigned a value but never used.  @typescript-eslint/no-unused-vars

--- src/app/api/fridge-account/route.ts
+++ src/app/api/fridge-account/route.ts
@@ -1,26 +1,29 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { createId } from '@paralleldrive/cuid2';
-export async function POST(req:Request,res:Response){
+import { createId } from "@paralleldrive/cuid2";
+export async function POST(req: Request) {
   const { userId, name, description } = await req.json();
-  try{
+  try {
     const fridgeId = createId();
-    const fridge = await prisma.fridge.create({
-      data:{
-        id:fridgeId,
-        name:name,
-        description:description
-      }
+    await prisma.fridge.create({
+      data: {
+        id: fridgeId,
+        name: name,
+        description: description,
+      },
     });
-    const userFridge = await prisma.userFridge.create({
-      data:{
-        userId:userId,
-        fridgeId:fridgeId
-      }
+    await prisma.userFridge.create({
+      data: {
+        userId: userId,
+        fridgeId: fridgeId,
+      },
     });
 
-    return NextResponse.json({ message: "Success", fridgeId:fridgeId}, { status: 201 });
-  }catch(err){
+    return NextResponse.json(
+      { message: "Success", fridgeId: fridgeId },
+      { status: 201 }
+    );
+  } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 }
