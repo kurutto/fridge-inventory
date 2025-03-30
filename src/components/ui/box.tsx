8d1baca08bc5b495d252a0ccfda3d02380fbd3f2
kurutto <namihojo@gmail.com>
2025-03-30 23:48:07 +0900

    余白を統一するためboxコンポーネントに余白専用のバージョンを追加

--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -2,14 +2,16 @@ import { cn } from "@/lib/utils";
 
 interface BoxProps
   extends Omit<React.ComponentPropsWithoutRef<"div">, "className"> {
-  variant?: "rounded";
+  variant?: "rounded" | "horizontally" | "spaceY";
   className?: string;
 }
 
 const Box = ({ variant, className, ...props }: BoxProps) => {
   const baseStyle = cn(
     variant === "rounded" &&
-      "bg-white md:rounded-[20px] md:p-8 md:space-y-8 md:shadow-pc max-md:rounded-2xl max-md:px-4 max-md:py-6 max-md:space-y-6 max-md:shadow-sp"
+      "bg-white md:rounded-[20px] md:p-8 md:space-y-8 md:shadow-pc max-md:rounded-2xl max-md:px-4 max-md:py-6 max-md:space-y-6 max-md:shadow-sp",
+      variant === "horizontally" && "sm:flex sm:items-center sm:space-x-4 max-sm:space-y-2",
+      variant === "spaceY" && 'space-y-4',
   );
   return <div className={cn(baseStyle, className)} {...props} />;
 };
