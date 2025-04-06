79689d02e4c15f1de7f9a46ca0c58a0b3292671d
kurutto <namihojo@gmail.com>
2025-04-06 20:21:12 +0900

    アカウントトップでの購入履歴のソートは不要なので削除

--- src/app/member/[fridgeId]/page.tsx
+++ src/app/member/[fridgeId]/page.tsx
@@ -3,11 +3,7 @@ import { nextAuthOptions } from "@/lib/next-auth/options";
 import { redirect } from "next/navigation";
 import Box from "@/components/ui/box";
 import Heading from "@/components/ui/heading";
-import {
-  FaListUl,
-  FaCubesStacked,
-  FaBagShopping,
-} from "react-icons/fa6";
+import { FaListUl, FaCubesStacked, FaBagShopping } from "react-icons/fa6";
 import AddToListButton from "@/components/shopping-list/add-to-list-button";
 import ShoppingList from "@/components/shopping-list/shopping-list";
 import InventoryTable from "@/components/inventory/inventory-table";
@@ -33,19 +29,8 @@ const FridgePage = async () => {
   const shoppingList = await getShoppingList(fridgeId);
   const inventories = await getInventories(fridgeId);
   const purchases = await getPurchases(fridgeId);
-  const sortedPurchases:PurchaseType[] = [...purchases];
-  sortedPurchases.sort((first, second) => {
-    if (first.purchaseDate > second.purchaseDate) {
-      return -1;
-    } else if (second.purchaseDate > first.purchaseDate) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
   const now = new Date();
   const fridgeAccountUsers = await getFridgeAccountUsers(fridgeId);
-  console.log('fridgeAccountUsers',fridgeAccountUsers)
   return (
     <>
       <Box variant="rounded">
@@ -55,7 +40,11 @@ const FridgePage = async () => {
           </Heading>
           <AddToListButton />
         </div>
-        <ShoppingList userId={userId} fridgeId={fridgeId} shoppingList={shoppingList} />
+        <ShoppingList
+          userId={userId}
+          fridgeId={fridgeId}
+          shoppingList={shoppingList}
+        />
       </Box>
       <Box variant="spaceY">
         <Heading outline={true} level={2} icon={FaCubesStacked}>
@@ -74,9 +63,15 @@ const FridgePage = async () => {
           </Heading>
           <AddPurchaseButton />
         </div>
-        <PurchaseList userId={userId} fridgeId={fridgeId} date={now} purchases={sortedPurchases} users={fridgeAccountUsers} headingStyle="max-md:text-center" />
+        <PurchaseList
+          userId={userId}
+          fridgeId={fridgeId}
+          date={now}
+          purchases={purchases}
+          users={fridgeAccountUsers}
+          headingStyle="max-md:text-center"
+        />
       </Box>
-
     </>
   );
 };
