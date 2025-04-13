8686a07fa5b05814d47f002283c023a80c7fde78
kurutto <namihojo@gmail.com>
2025-04-13 12:16:20 +0900

    買物リストの文言修正

--- src/components/shopping-list/shopping-list.tsx
+++ src/components/shopping-list/shopping-list.tsx
@@ -5,45 +5,55 @@ import Paragraph from "../ui/paragraph";
 
 interface ShoppingListProps {
   userId: string;
-  fridgeId:string;
+  fridgeId: string;
   shoppingList: ShoppingListType[];
 }
 
-const ShoppingList = async ({ userId, fridgeId, shoppingList }: ShoppingListProps) => {
+const ShoppingList = async ({
+  userId,
+  fridgeId,
+  shoppingList,
+}: ShoppingListProps) => {
   return (
     <>
-    {shoppingList.length === 0 ? <Paragraph>リストは登録されていません</Paragraph>:
-      <List space="none" className="leading-[1.1] -mt-2.5">
-        {shoppingList.map((item, idx) => (
-          <Li key={idx} className="relative pr-10 pt-2.5">
-            ・{item.name}
-            {item.amount && (
-              <span className="text-sm pl-1 leading-none">{item.amount}</span>
-            )}
-            <br />
-            <span className="text-xs text-gray">
-              {new Date(item.createdAt).toLocaleDateString("en-US", {
-                month: "numeric",
-                day: "numeric",
-              })}
-              {item.dueDate && (
-                <span className="text-xs text-gray pl-0.5">
-                  (
-                  {new Date(item.dueDate).toLocaleDateString("en-US", {
-                    month: "numeric",
-                    day: "numeric",
-                  })}
-                  まで)
-                </span>
+      {shoppingList.length === 0 ? (
+        <Paragraph>買物リストは登録されていません</Paragraph>
+      ) : (
+        <List space="none" className="leading-[1.1] -mt-2.5">
+          {shoppingList.map((item, idx) => (
+            <Li key={idx} className="relative pr-10 pt-2.5">
+              ・{item.name}
+              {item.amount && (
+                <span className="text-sm pl-1 leading-none">{item.amount}</span>
               )}
-            </span>
-            <span className="text-xs text-gray pl-1">{item.user?.name}</span>
-            {item.userId === userId && (
-              <RemoveFromListButton fridgeId={fridgeId} listItemId={item.id} />
-            )}
-          </Li>
-        ))}
-      </List>}
+              <br />
+              <span className="text-xs text-gray">
+                {new Date(item.createdAt).toLocaleDateString("en-US", {
+                  month: "numeric",
+                  day: "numeric",
+                })}
+                {item.dueDate && (
+                  <span className="text-xs text-gray pl-0.5">
+                    (
+                    {new Date(item.dueDate).toLocaleDateString("en-US", {
+                      month: "numeric",
+                      day: "numeric",
+                    })}
+                    まで)
+                  </span>
+                )}
+              </span>
+              <span className="text-xs text-gray pl-1">{item.user?.name}</span>
+              {item.userId === userId && (
+                <RemoveFromListButton
+                  fridgeId={fridgeId}
+                  listItemId={item.id}
+                />
+              )}
+            </Li>
+          ))}
+        </List>
+      )}
     </>
   );
 };
