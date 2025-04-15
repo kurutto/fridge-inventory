3585f012e2191163463828e7bf8e5732ecd90fc9
kurutto <namihojo@gmail.com>
2025-04-15 09:59:23 +0900

    purchasesのusersオブジェクト配列作成をsomeを使った方法に変更

--- src/lib/purchase.ts
+++ src/lib/purchase.ts
@@ -1,6 +1,8 @@
 import { PurchasesUserType, PurchaseType } from "@/types/types";
 
-export const getPurchases = async ( fridgeId:string ): Promise<PurchaseType[]> => {
+export const getPurchases = async (
+  fridgeId: string
+): Promise<PurchaseType[]> => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/fridge/${fridgeId}/purchase`
   );
@@ -8,25 +10,14 @@ export const getPurchases = async ( fridgeId:string ): Promise<PurchaseType[]> =
     throw new Error("Failed to fetch purchases");
   }
   return await res.json();
-}
+};
 
-export const getPurchasesUsers = (purchases:PurchaseType[]) => {
-  const sortedPurchasesByUser: PurchaseType[] = [...purchases];
-  sortedPurchasesByUser.sort((first, second) => {
-    if (first.userId > second.userId) {
-      return -1;
-    } else if (second.userId > first.userId) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
+export const getPurchasesUsers = (purchases: PurchaseType[]) => {
   const users: PurchasesUserType[] = [];
-  sortedPurchasesByUser.forEach((purchase, idx) => {
-    if (idx === 0 || purchase.userId != sortedPurchasesByUser[idx - 1].userId) {
+  purchases.forEach((purchase) => {
+    if (!users.some((user) => user.id === purchase.userId)) {
       users.push({ id: purchase.userId, name: purchase.user.name });
     }
   });
   return users;
-
-}
+};
