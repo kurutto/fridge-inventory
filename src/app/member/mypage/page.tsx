940cb18711ceb2c0da11d8428cbf9f583ee75d43
kurutto <namihojo@gmail.com>
2025-04-29 19:36:37 +0900

    余白統一のためのスタイル変更

--- src/app/member/mypage/page.tsx
+++ src/app/member/mypage/page.tsx
@@ -16,7 +16,7 @@ const MyPage = async () => {
   return (
     <>
       <Heading level={1}>マイページ</Heading>
-      <div className="md:flex md:gap-7 max-md:space-y-6">
+      <div className="md:flex md:gap-x-11 max-md:space-y-6">
         <Box variant="rounded" className="flex-1/2">
           <Heading level={2} className="justify-center">
             ユーザー情報
