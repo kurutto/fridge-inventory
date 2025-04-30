b632fe8ed0c2c05e2ecd0ca6410076d379d0ae56
kurutto <namihojo@gmail.com>
2025-04-30 11:10:50 +0900

    ユーザーアカウントを削除した時他のデバイスでsessionが残っているためのエラーを修正

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -3,7 +3,8 @@ import { UserType } from '@/types/types';
 export const getUser = async (userId: string): Promise<UserType> => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/${userId}`);
   if (!res.ok) {
-    throw new Error("Failed to fetch fridge accounts");
-  }
+    const data = await res.json();
+    alert(data.message);
+  } 
   return await res.json();
 };
